e12c5c8bdfba0bba0c2e9bb6ffe01f559dbf1636
Michael Chernysh <chermishan@gmail.com>
2025-09-10 13:30:49 +0300

    Added C# Web API

---
+++ API/KotlinBasics_API/KotlinBasics_API/Program.cs
@@ -0,0 +1,25 @@
+using KotlinBasics_API.Module_1_Example_9;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5156); // порт для HTTP
+});
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+SolarForecastAPI.Initialize(app);
+
+
+app.Run();
+
